d087d01923a805b171f2a6cd360b62a0357d9d32
BhoomiKesarwani <kesarwanibhoomi12@gmail.com>
2026-06-21 15:26:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,21 +3,6 @@ import pickle
 import pandas as pd
 import requests
 
-# def fetch_poster(movie_id):
-#     response=requests.get()
-#     data=response.json()
-#     return data['poster_path']
-
-# def fetch_poster(movie_id):
-#     url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
-#     data = requests.get(url)
-#     data = data.json()
-#     poster_path = data['poster_path']
-#     full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
-#     return full_path
-
-import requests
-
 def fetch_poster(movie_id):
     url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US"
 
@@ -45,39 +30,23 @@ def recommend(movie):
     recommended_movie_posters=[]
   
     for i in movies_list[1:6]:
-        #movie_id=i[0]
-        #fetch poster from API
         movie_id = movies.iloc[i[0]].movie_id
         recommended_movie_posters.append(fetch_poster(movie_id))
         recommended_movie_names.append(movies.iloc[i[0]].title)
 
-        #recommended_movies.append(movies.iloc[i[0]].title)
     return recommended_movie_names,recommended_movie_posters
 
-
-#st.header('Movie Recommender System')    
 movies_dict=pickle.load(open('movie_dict.pkl','rb'))
 movies=pd.DataFrame(movies_dict)
 
 similarity=pickle.load(open('similarity.pkl','rb'))
 st.title('Movies Recommender System')
 
-
-#selected_movie_name = st.selectbox(
- #   "Choose your favorite genre:",
-  #  movies['title'].values
-#)
-
 movie_list = movies['title'].values
 selected_movie = st.selectbox(
     "Type or select a movie from the dropdown",
     movie_list
 )
-
-# if st.button("Recommend"):
-#     recommendations=recommend(selected_movie)
-#     for i in recommendations:
-#         st.write(i)
 
 if st.button('Show Recommendation'):
     recommended_movie_names,recommended_movie_posters = recommend(selected_movie)
